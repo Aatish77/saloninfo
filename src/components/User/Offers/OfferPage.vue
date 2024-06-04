8ca8ecd8e44dd91a859d42f36f06fe3500eaee68
aatish877 <91455131+Aatish77@users.noreply.github.com>
2024-06-04 11:06:00 +0530

    aa

--- src/components/User/Offers/OfferPage.vue
+++ src/components/User/Offers/OfferPage.vue
@@ -85,8 +85,8 @@ export default {
       carouselItems: [
         { src: 'https://www.stryx.com/cdn/shop/articles/how-often-men-should-get-haircuts.jpg?v=1644630261', title: 'Hair cut 50% OFF' },
         { src: 'https://houseofdearhairsalon.com/wp-content/uploads/2022/04/5-Salon-Hair-Treatments-to-Fix-Your-Hair-From-Root-to-Tip-1024x576.jpg', title: 'Hair Treatment 25% OFF' },
-        { src: 'https://d1ooscleda9ip9.cloudfront.net/Upload/687/CMS/News/Photos/8f1d0233-942.jpg', title: 'Skin Care 30% OFF' },
-        { src: 'https://mjgorgeous.com/wp-content/uploads/2020/12/MACost2.jpg', title: 'Bridal Makeup 20% OFF' },
+        { src: 'https://media.istockphoto.com/id/980122828/photo/young-man-applying-anti-aging-lotion-fot-skin-care.jpg?s=612x612&w=0&k=20&c=wyfFgr-9DuZcbRh-0DHD8elICDYE-mlNk92YSTXCzaw=', title: 'Skin Care 30% OFF' },
+        { src: 'https://media.istockphoto.com/id/980122828/photo/young-man-applying-anti-aging-lotion-fot-skin-care.jpg?s=612x612&w=0&k=20&c=wyfFgr-9DuZcbRh-0DHD8elICDYE-mlNk92YSTXCzaw=', title: 'Bridal Makeup 20% OFF' },
         { src: 'https://images.travelandleisureasia.com/wp-content/uploads/sites/2/2022/02/02132521/shutterstock_420858511.jpg?tr=w-1200,q-60', title: 'Tattoo 20% OFF' }
       ]
     };
