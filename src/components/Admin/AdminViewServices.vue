1a2b105fe157bebf5acab2019505f1333d285f32
aatish877 <91455131+Aatish77@users.noreply.github.com>
2024-06-07 15:47:01 +0530

    admin

--- src/components/Admin/AdminViewServices.vue
+++ src/components/Admin/AdminViewServices.vue
@@ -1,3 +1,91 @@
 <template>
-    <h1>Services</h1>
-</template>
+    <v-app id="inspire">
+      
+  
+      <v-app-bar>
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+  
+        <v-app-bar-title>Application</v-app-bar-title>
+      </v-app-bar>
+  
+      <v-navigation-drawer
+        v-model="drawer"
+        temporary
+      > <v-list density="compact" nav>
+          <v-list-item
+            prepend-icon="mdi-home-city"
+            title="Overview"
+            value="overview"
+          ></v-list-item>
+          <v-list-item
+            prepend-icon="fas fa-briefcase"
+            title="Services"
+            value="account"
+            @click="serviceClick"
+          ></v-list-item>
+          <v-list-item
+            prepend-icon="mdi-account"
+            title="Pending Requests"
+            value="account"
+            @click="clickProfile"
+          ></v-list-item>
+          <v-list-item
+            prepend-icon="fas fa-tags"
+            title="List of parlours"
+            value="account"
+            @click="offerClick"
+          ></v-list-item>
+         
+          
+          
+        </v-list>
+        <template v-slot:append>
+          <div class="pa-2">
+            <v-btn block @click="logout"> Logout </v-btn>
+          </div>
+        </template>
+        <!--  -->
+      </v-navigation-drawer>
+  
+      <v-main class="bg-grey-lighten-2">
+        <v-card>
+    <v-tabs v-model="tab" align-tabs="center" color="deep-purple-accent-4">
+     
+      <v-tab :value="2">Men</v-tab>
+      <v-tab :value="1">Women</v-tab>
+    </v-tabs>
+
+    <v-tabs-window v-model="tab">
+      <v-tabs-window-item v-for="n in 3" :key="n" :value="n">
+        <v-container fluid>
+          <v-row>
+            <v-col v-for="i in 6" :key="i" cols="12" md="4">
+              <v-img
+                :lazy-src="`https://picsum.photos/10/6?image=${i * n * 5 + 10}`"
+                :src="`https://picsum.photos/500/300?image=${i * n * 5 + 10}`"
+                height="205"
+                cover
+              ></v-img>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-tabs-window-item>
+    </v-tabs-window>
+  </v-card>
+      </v-main>
+    </v-app>
+  </template>
+  
+  <script setup>
+    import { ref } from 'vue'
+  
+    const drawer = ref(null)
+  </script>
+  
+  <script>
+    export default {
+      data: () => ({ drawer: null,
+        tab: null,
+       }),
+    }
+  </script>
