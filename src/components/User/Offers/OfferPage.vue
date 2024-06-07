7c2f7871c670271dc39e3bd479d431ae8993d401
aatish877 <91455131+Aatish77@users.noreply.github.com>
2024-06-07 13:36:11 +0530

    aa

--- src/components/User/Offers/OfferPage.vue
+++ src/components/User/Offers/OfferPage.vue
@@ -1,6 +1,5 @@
 <template>
-  <v-card style="background-color: black; color: white;">
-    <v-layout>
+  <v-app style="background-color: black; color: white;">
     <v-app-bar app fixed style="background-image: linear-gradient(135deg, transparent 0%, transparent 50%,rgba(159, 159, 159,0.07) 50%, rgba(159, 159, 159,0.07) 77%,transparent 77%, transparent 100%),linear-gradient(90deg, transparent 0%, transparent 91%,rgba(159, 159, 159,0.07) 91%, rgba(159, 159, 159,0.07) 99%,transparent 99%, transparent 100%),linear-gradient(135deg, transparent 0%, transparent 24%,rgba(159, 159, 159,0.07) 24%, rgba(159, 159, 159,0.07) 63%,transparent 63%, transparent 100%),linear-gradient(0deg, transparent 0%, transparent 49%,rgba(159, 159, 159,0.07) 49%, rgba(159, 159, 159,0.07) 63%,transparent 63%, transparent 100%),linear-gradient(90deg, rgb(0,0,0),rgb(0,0,0));"
      color="teal-darken-4">
       <template v-slot:image>
@@ -70,8 +69,7 @@
         </v-col>
       </v-row>
     </v-main>
-  </v-layout>
-  </v-card>
+  </v-app>
   <v-card>
     <v-layout>
       <v-navigation-drawer v-model="drawer" temporary location="right" style="background-color: black; color:white; z-index: 4;">
